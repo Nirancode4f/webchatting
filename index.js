8ba77515c72aa19f845192546b124dfdb3241869
eleMarkuspet <>
2021-12-11 17:33:00 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//connect to html
+
+//print server request
+
+const http = require('http');
+const server = http.createServer(app);
+const {
+    Server
+} = require('socket.io');
+
+
+const io = new Server(server);
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/src/client.html");
+})
+
+
+
+io.on("connection", socket => {
+    console.log("user connected")
+    io.emit("nlogin",
+        ">>new user login<<"
+    )
+
+    socket.on("on-chat", data => {
+        console.log(data)
+        io.emit("user-chat", data)
+    })
+})
+
+io.on("connection", socket => {
+    console.log("user disconnected")
+})
+
+server.listen(port, () => {
+    console.log('connect on 3000')
+});
